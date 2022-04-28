3958e09fdaa1e568f670f4a5c4880b9fd3edcfcb
Luke <28629535+2spoopy4me@users.noreply.github.com>
2022-04-27 20:23:24 -0500

    1st commit

---
+++ dip/dip.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3.
+# database interface program
+
+'''16gb db is too big to load into memory, need
+to figure out how you can parse and load into memory.
+Start by sifting through and only selecting column
+names - from there you can spider the db, only adding
+to memory/dictionary what values are from the column
+specified'''
+
+
+class Dip:
+    def __init__(self, filename):
+        self.filename = filename
+
